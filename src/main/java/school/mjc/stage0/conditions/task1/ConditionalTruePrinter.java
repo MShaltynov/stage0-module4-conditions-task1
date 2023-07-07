d92cf9ee5bc93e2f04b6c50ace7ecd1b96cafb14
Mikhail <shaltynovm@gmail.com>
2023-07-07 16:25:25 +0530

    initial commit

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -1,6 +1,15 @@
 package school.mjc.stage0.conditions.task1;
 
+import java.sql.SQLOutput;
+
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+        if (a < b) {
+            System.out.println(true);
+        } else {
+            System.out.println(true);
+        }
     }
 }
